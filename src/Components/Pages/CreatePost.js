00ff1d2116d5680b18970b3a17d601920ab53952
jonathanyu1 <jonathanyusir@gmail.com>
2021-05-17 16:13:31 -0400

    added img file upload preview on creating post

--- src/Components/Pages/CreatePost.js
+++ src/Components/Pages/CreatePost.js
@@ -7,6 +7,9 @@ const CreatePost = (props) => {
     const [communityNames, setCommunityNames] = useState([]);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [imgFile, setImgFile] = useState(null);
+    const [imgFileSrc, setImgFileSrc] = useState(null);
+    // const [imgFileName, setImgFileName] = useState('');
     const [communitySelect, setCommunitySelect] = useState('');
     const [postId, setPostId] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
@@ -73,6 +76,7 @@ const CreatePost = (props) => {
             console.log('hi');
             addPostToFs()
             .then((data)=>{
+                URL.revokeObjectURL(imgFileSrc);
                 console.log(data.id);
                 setPostId(data.id);
                 setIsRedirect(true);
@@ -98,6 +102,20 @@ const CreatePost = (props) => {
                 console.log(e.target.value);
                 setCommunitySelect(e.target.value);
                 break;
+        }
+    }
+
+    const handleChangeImage = (e) => {
+        if (e.target.files && e.target.files[0]){
+            console.log(e);
+            console.log(e.target.name);
+            console.log(e.target.value);
+            console.log(e.target.files);
+            console.log(e.target.files[0]);
+            console.log(e.target.files[0].name);
+            setImgFile(e.target.files[0]);
+            setImgFileSrc(URL.createObjectURL(e.target.files[0]));
+            // setImgFileName(e.target.files[0].name);
         }
     }
 
@@ -136,6 +154,18 @@ const CreatePost = (props) => {
                                 onChange={handleChange}
                                 value={description}
                             />
+                        <div>
+                            {/* <label htmlFor='createPostImgUpload' className='labelUploadImg'>Upload an image: </label> */}
+                            <input 
+                                type='file' 
+                                accept='image/*'
+                                id='createPostImgUpload'
+                                onChange={handleChangeImage}
+                            />
+                            {imgFile && imgFileSrc &&
+                                <img src={`${imgFileSrc}`} alt={`${imgFile.name}`} className='imgUploadPreview'/>
+                            }
+                        </div>
                         </div>
                             <label htmlFor='selectCommunityNames' className='labelPostCommunityNames'>Select a community for your post: </label>
                             {isLoading ? 
