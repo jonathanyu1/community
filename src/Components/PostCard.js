7da467205d295d238a0400de521aeebbec39a6a0
jonathanyu1 <jonathanyusir@gmail.com>
2021-05-17 21:21:05 -0400

    setup expanding and contracting image/descriptions on community pages

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -4,9 +4,9 @@ import {calcTimeSincePosted} from './Helpers/helperFunctions';
 
 const PostCard = (props) => {
     const [timeSincePost, setTimeSincePost] = useState('');
+    const [showInfo, setShowInfo] = useState(false);
 
     const calculateTime = () => {
-        // need to finish this
         setTimeSincePost(calcTimeSincePosted(props.post.createdTimestamp.seconds));
         // setTimeSincePost(`${props.post.createdTimestamp.seconds} seconds ago`);
     }
@@ -23,29 +23,57 @@ const PostCard = (props) => {
                     {props.post.title}
                 </Link>
             </div>
-            <div className='postCardSubmission'>
-                {`Submitted ${timeSincePost} ago by `}
-                {
-                    <Link to={`/user/${props.post.userCreator}`}>
-                        {props.post.userCreator}
-                    </Link>
+            <div className='postCardDetails'>
+                {props.post.imgUrl && 
+                <React.Fragment>
+                    <i className={`fa fa-times ${showInfo ? 'show' : 'hide' }`} aria-hidden="true" onClick={()=>{setShowInfo(false)}}></i>
+                    <i className={`fa fa-file-image-o ${showInfo ? 'hide' : 'show' }`} aria-hidden="true" onClick={()=>{setShowInfo(true)}}></i>
+                </React.Fragment>
                 }
-                {` to `}
-                {
-                    <Link to={`/c/${props.post.community}`}>
-                        /c/{props.post.community}
-                    </Link>
+                {props.post.description && !props.post.imgUrl &&
+                <React.Fragment>
+                    <i className={`fa fa-times ${showInfo ? 'show' : 'hide' }`} aria-hidden="true" onClick={()=>{setShowInfo(false)}}></i>
+                    <i className={`fa fa-list ${showInfo ? 'hide' : 'show' }`} aria-hidden="true" onClick={()=>{setShowInfo(true)}}></i>
+                </React.Fragment>
                 }
+                <div className='postCardDetailsRight'>
+                    <div className='postCardSubmission'>
+                        {`Submitted ${timeSincePost} ago by `}
+                        {
+                            <Link to={`/user/${props.post.userCreator}`}>
+                                {props.post.userCreator}
+                            </Link>
+                        }
+                        {` to `}
+                        {
+                            <Link to={`/c/${props.post.community}`}>
+                                /c/{props.post.community}
+                            </Link>
+                        }
+                    </div>
+                    <div className='postCardLinks'>
+                        <Link to={`/c/${props.post.community}/${props.post.postId}`}>
+                            {/* Need to figure out counting nesting comments here later */}
+                            {`Comments`}
+                        </Link>
+                        {/* <div onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}}>
+                            Share
+                        </div> */}
+                    </div>
+                </div>
             </div>
-            <div className='postCardLinks'>
-                <Link to={`/c/${props.post.community}/${props.post.postId}`}>
-                    {/* Need to figure out counting nesting comments here later */}
-                    {`Comments`}
-                </Link>
-                {/* <div onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}}>
-                    Share
-                </div> */}
+            {props.post.imgUrl && 
+            <img 
+                className={`postPageImg ${showInfo ? 'show' : 'hide' }`} 
+                src={props.post.imgUrl} 
+                alt={props.post.title}
+            />
+            }
+            {props.post.description &&
+            <div className={`postCardDescription ${showInfo ? 'show' : 'hide' }`} >
+                {props.post.description}
             </div>
+            }
         </div>
     )
 }
