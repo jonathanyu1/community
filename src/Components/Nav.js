4d3b408b0c0c5c9791c2b8eb459f5af91b702558
jonathanyu1 <jonathanyusir@gmail.com>
2021-05-31 15:43:12 -0400

    setup nav for mobile viewing

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import communityHeader from '../Images/Header/communityHeader.png';
 
 const Nav = (props) => {
 
@@ -8,7 +9,14 @@ const Nav = (props) => {
             <nav className='nav'>
                 <div id='navHeaderContainer'>
                     <Link to='/'>
-                        <div id='navHeader'>Community</div>
+                        <img 
+                            className='navHeaderIcon'
+                            src={communityHeader}
+                            alt='community header icon'
+                        />
+                    </Link>
+                    <Link to='/'>
+                        <div id='navHeader' className='navText'>ommunity</div>
                     </Link>
                 </div>
                 <ul className='navTabs'>
@@ -39,7 +47,7 @@ const Nav = (props) => {
                             <Link to={`/user/${props.displayName}`}>
                                 <li className='navProfileInfo'>
                                     <img src={props.profilePicUrl} alt='profilepic' className='navProfilePic'/>
-                                    {props.displayName}
+                                    <li className='navText'>{props.displayName}</li>
                                 </li>
                             </Link>
                             <Link to='/settings'>
@@ -50,16 +58,28 @@ const Nav = (props) => {
                                 </li>
                             </Link>
                             <Link to='/' >
-                                <li onClick={props.signOut}>Sign out</li>
+                                <li className='navText' onClick={props.signOut}>Sign out</li>
+                                <span className="material-icons-outlined navIconMobile" onClick={props.signOut}>
+                                    logout
+                                </span>
                             </Link>
                         </React.Fragment>
                     : 
                         <React.Fragment>
                             <Link to='/signup' >
-                                <li>Sign Up</li>
+                                <li className='navText'>Sign Up</li>
+                                <span className="material-icons-outlined navIconMobile">
+                                    person_add
+                                </span>
                             </Link>
                             <Link to='/signin'>
-                                <li>Sign In</li>
+                                <li className='navText'>Sign In</li>
+                                <span className="material-icons-outlined navIconMobile">
+                                    login
+                                </span>
+                            </Link>
+                            <Link to='/signin' >
+                                
                             </Link>
                         </React.Fragment>
                     }
